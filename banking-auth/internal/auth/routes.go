bcb2af18d8ff097ac346bc190f68ba5be8c572bb
ani213 <vermaaniket213@gmail.com>
2025-12-16 21:28:13 +0530

    middleware added for json Response

--- banking-auth/internal/auth/routes.go
+++ banking-auth/internal/auth/routes.go
@@ -1,18 +1,23 @@
 package auth
 
 import (
+	"github.com/ani213/banking-auth/internal/middleware"
 	"github.com/gorilla/mux"
 )
 
 func PublicRoutes(r *mux.Router, h *Handler) {
-	r.HandleFunc("/register", h.Register).Methods("POST")
-	r.HandleFunc("/login", h.Login).Methods("POST")
+	api := r.PathPrefix("/").Subrouter()
+	api.Use(middleware.JSONMiddleware)
+	api.HandleFunc("/register", h.Register).Methods("POST")
+	api.HandleFunc("/login", h.Login).Methods("POST")
 
 }
 
 func PrivateRoutes(r *mux.Router, h *Handler) {
-	r.HandleFunc("/users", h.GetUsers).Methods("GET")
-	r.HandleFunc("/context", h.GetContext).Methods("GET")
-	r.HandleFunc("/validate-token", h.ValidateToken).Methods("GET")
-	r.HandleFunc("/add-role", h.AddUserRole).Methods("PUT")
+	api := r.PathPrefix("/api").Subrouter()
+	api.Use(middleware.JSONMiddleware)
+	api.HandleFunc("/context", h.GetContext).Methods("GET")
+	api.HandleFunc("/users", h.GetUsers).Methods("GET")
+	api.HandleFunc("/validate-token", h.ValidateToken).Methods("GET")
+	api.HandleFunc("/add-role", h.AddUserRole).Methods("PUT")
 }
